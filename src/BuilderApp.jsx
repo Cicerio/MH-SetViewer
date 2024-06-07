bb2d2dc29343f278f8fd831fb86721291d371ed7
Ralph L Robinson <ralphrobinson@student.olympic.edu>
2024-06-07 16:03:47 -0700

    Redoing the site, still working on frontend

--- src/BuilderApp.jsx
+++ src/BuilderApp.jsx
@@ -1,17 +1,69 @@
 import './css/builderApp.css'
+import { useState, useEffect } from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 export default function BuilderApp() {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 700) {
+        setSelectedTab(2);
+      } else {
+        setSelectedTab(0);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  const isEquipmentSelected = selectedTab >= 0;
+  const isStatsSelected = selectedTab >= 1;
+  const isSkillsSelected = selectedTab >= 2;
 
   return (
-    <section className='body'>
-      hello world, this is the mhrise thing
-      <main>This is the main box
-        <section className='gearView'>
-          <div className='gear'>gear box
-            <div className='weapon.select'>weapon box</div>
-            <div className='armor.head.select emphasis'>head box</div>
-          </div>
+    <main className='container'>
+      <div className='tab-container'>
+        <div className={`tab ${selectedTab >= 0 ? 'selected' : ''}`}>Equipment</div>
+        <div className={`tab ${selectedTab >= 1 ? 'selected' : ''}`}>Stats</div>
+        <div className={`tab ${selectedTab >= 2 ? 'selected' : ''}`}>Skills</div>
+      </div>
+      {isEquipmentSelected && (
+        <section className='gear-container'>
+          gear
         </section>
-      </main>
-    </section>
+      )}
+      {isStatsSelected && (
+        <section className='stats-container'>
+          <div className='container' >
+            <h4>Attack</h4>
+            <div>
+              <ul className='attack-stats striped'>
+                {/* maybe have these blocks be their own components */}
+              </ul>
+            </div>
+            <br />
+            <h4>Defense</h4>
+            <div></div>
+            <br />
+          </div>
+          {isSkillsSelected && (
+            <section className='skills-container'>
+              <div className='container' >
+                <br />
+                <h4>
+                  Skills 
+                </h4>
+              </div>
+            </section>
+          )}
+        </section>
+      )}
+    </main>
   );
 }
